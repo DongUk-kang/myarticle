bd2ff369bfdcd088155bee97bf0ba1f21a521071
dong uk <gusrn1255@naver.com>
2021-01-20 12:27:55 +0900

    map함수 되는지 안되는지 시도해봄
    (api구성이 배열안에 각각 배열(?)이 더있어서 안되는걸로추측)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+
+    //1. 데이터 담을공간
+    const [articles, setArticles] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    //3. 네트워킹
+
+    const getData = async () => {
+        return (
+            await axios.get('http://www.findyourapi.com/api/posts/')
+                // .then(res =>console.log(res.data))
+                .then(res => {
+                    setArticles(res.data)
+                    setLoading(false)
+                })
+                .catch(err => console.log(err))
+        )
+    }
+
+
+
+
+    //2, 자동실행 함수
+    useEffect(() => {
+        getData()
+    }, {})
+
+
+    return (
+        <>
+            {loading ?
+                <div>
+                    <h1>
+                        loading ...
+                    </h1>
+                </div>
+                : (
+
+                    <div>
+                        {articles.map(data =>
+                        <>
+                            <h1>Article author : {data.author_name}</h1>
+                            <h2>Article title : {data.title}</h2>
+                            <h3>content of the article : {data.content}</h3>
+                            <h4>Article writing date : {data.published_at}</h4>
+                        </>
+                        )}
+                    </div>
+                )
+            }
+                </>
+                )
+
+
+
+};
 export default App;
